2032ab1dbed5bdd46c74841348e4392872920247
wuhaixing <wuhaixing@gmail.com>
2013-03-25 23:11:19 +0800

    change to awestruct

---
+++ _ext/pipeline.rb
@@ -0,0 +1,22 @@
+require 'tagger_sanitizer'
+require 'atomizer_custom'
+
+Awestruct::Extensions::Pipeline.new do
+  # extension Awestruct::Extensions::Posts.new( '/news' ) 
+  extension Awestruct::Extensions::Posts.new('/blog', :posts)
+  extension Awestruct::Extensions::Paginator.new( :posts, '/blog/index', :per_page=>10 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>10, :sanitize=>true )
+  extension Awestruct::Extensions::TagCloud.new(:posts, '/blog/tags/index.html', :layout=>'tab', :title=>'Tags')
+  extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new( 
+    :posts, 
+    '/blog/feed-private.atom', 
+    :num_entries=>10000,
+    :content_url=>'http://cocasts.com/blog',
+    :feed_title=> '编程改变数据' )
+  extension Awestruct::Extensions::Flattr.new
+  extension Awestruct::Extensions::Disqus.new
+  helper Awestruct::Extensions::Partial
+  helper Awestruct::Extensions::GoogleAnalytics
+end
+
